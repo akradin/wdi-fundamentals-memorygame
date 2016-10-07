0a8792a7826616d5996b4bba7718731c17507b22
Alex Kradin <AlexKradin@MBP-Retina.local>
2016-10-07 11:52:03 -0400

    added some js

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,12 @@ var card0ne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if(card0ne===cardtwo){
+	alert('You found a match!';)
+}
+else if(cardThree===cardFour){
+	alert('You found a match';)
+}
+else{
+	alert('Try again!')
+}
